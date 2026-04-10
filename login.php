cb7e3d45a4b22570130774235153779b25875dfa
Vennela Raghava Konduru <vennelakonduru93@gmail.com>
2026-04-10 21:21:08 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,13 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <link rel="stylesheet" href="style.css">
-</head>
+<head><link rel="stylesheet" href="style.css"></head>
 <body>
     <header><a href="index.php">HOME</a></header>
     <div class="container" style="max-width: 400px;">
         <h2>Student Login</h2>
         <form action="instructions.php" method="POST">
-            <input type="text" name="uname" placeholder="Enter Your Name" required>
-            <input type="password" name="upass" placeholder="Enter Password" required>
-            <button type="submit" class="btn">LOGIN</button>
+            <input type="text" name="uname" placeholder="Enter Name" required style="width:100%; padding:10px; margin:10px 0;">
+            <input type="password" name="upass" placeholder="Enter Password" required style="width:100%; padding:10px; margin:10px 0;">
+            <button type="submit" class="btn" style="width:100%;">LOGIN</button>
         </form>
     </div>
 </body>
