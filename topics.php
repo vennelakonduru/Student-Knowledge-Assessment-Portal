15e8fa2a806c55dac3a003551f53f389f991c157
Vennela Raghava Konduru <vennelakonduru93@gmail.com>
2026-04-10 20:23:05 +0530

    Update topics.php

--- topics.php
+++ topics.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>Select Topic</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header><a href="index.php">HOME</a></header>
     <div class="container">
-        <h2>Choose Your Subject</h2>
-        <a href="quiz.php?topic=HTML5" class="btn">HTML5 Assessment</a>
-        <a href="quiz.php?topic=CSS3" class="btn">CSS3 Assessment</a>
+        <h2 style="color: #2c3e50;">Choose Your Subject</h2>
+        <a href="quiz.php?topic=HTML" class="btn">HTML Assessment</a>
+        <a href="quiz.php?topic=CSS" class="btn">CSS Assessment</a>
         <a href="quiz.php?topic=JavaScript" class="btn">JavaScript Assessment</a>
     </div>
 </body>
