46c0301b0eea1125bc9c268c1aa0b9d956c30b36
Vennela Raghava Konduru <vennelakonduru93@gmail.com>
2026-04-10 20:24:21 +0530

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,35 +1,51 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
-<head><link rel="stylesheet" href="style.css"></head>
+<head>
+    <title>Quiz - SKAP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 <body>
-    <header><a href="index.php">HOME</a></header>
-    <div class="container" style="text-align: left;">
-        <h2 style="text-align: center;">Topic: <?php echo $_GET['topic']; ?></h2>
+    <header>
+        <a href="index.php">HOME</a>
+    </header>
+
+    <div class="container" style="max-width: 700px; text-align: left;">
+        <h2 style="text-align: center; color: #2c3e50;">Topic: <?php echo $_GET['topic']; ?></h2>
+        <hr>
+        
         <form action="result.php" method="POST">
             <input type="hidden" name="topic" value="<?php echo $_GET['topic']; ?>">
 
-            <div class="quiz-q">
-                <p><b>1. What does <?php echo $_GET['topic']; ?> primarily deal with?</b></p>
-                <input type="radio" name="q1" value="1" required> Structure/Logic <br>
-                <input type="radio" name="q1" value="0"> Hardware
+            <div style="margin-bottom: 25px;">
+                <p><b>1. What is the main purpose of <?php echo $_GET['topic']; ?>?</b></p>
+                <input type="radio" name="q1" value="1" required> Web Development <br>
+                <input type="radio" name="q1" value="0"> Data Entry <br>
+                <input type="radio" name="q1" value="0"> Graphic Design
             </div>
-            <div class="quiz-q">
-                <p><b>2. Is this a core web technology?</b></p>
-                <input type="radio" name="q2" value="1" required> Yes <br>
-                <input type="radio" name="q2" value="0"> No
+
+            <div style="margin-bottom: 25px;">
+                <p><b>2. Is <?php echo $_GET['topic']; ?> difficult to learn?</b></p>
+                <input type="radio" name="q2" value="1"> No, it is beginner-friendly <br>
+                <input type="radio" name="q2" value="0"> Yes, extremely hard <br>
+                <input type="radio" name="q2" value="0"> It depends on the day
             </div>
-            <div class="quiz-q">
-                <p><b>3. Is it maintained by W3C/ECMA?</b></p>
-                <input type="radio" name="q3" value="1" required> Yes <br>
-                <input type="radio" name="q3" value="0"> No
+
+            <div style="margin-bottom: 25px;">
+                <p><b>3. Do professional developers use <?php echo $_GET['topic']; ?>?</b></p>
+                <input type="radio" name="q3" value="1"> Yes, every day <br>
+                <input type="radio" name="q3" value="0"> No, never <br>
+                <input type="radio" name="q3" value="0"> Only for personal blogs
             </div>
-            <div class="quiz-q">
-                <p><b>4. Does it run on the client side?</b></p>
-                <input type="radio" name="q4" value="1" required> Yes <br>
-                <input type="radio" name="q4" value="0"> No
+
+            <div style="margin-bottom: 25px;">
+                <p><b>4. Is this assessment helpful?</b></p>
+                <input type="radio" name="q4" value="1"> Yes <br>
+                <input type="radio" name="q4" value="0"> No <br>
+                <input type="radio" name="q4" value="0"> Maybe
             </div>
-            <button type="submit" class="btn" style="width:100%">Submit My Answers</button>
+
+            <button type="submit" class="btn btn-green">Submit My Answers</button>
         </form>
     </div>
 </body>
